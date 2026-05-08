16c6dc08bcb9ed5c2dcdc2fb63d6a997d9cf5000
onyillto <56700691+onyillto@users.noreply.github.com>
2026-05-08 11:59:46 +0100

    ..

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SIcIrARjE3J4KEYw+STB5fAXYeTolBuMjBzhwLqNxTE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
